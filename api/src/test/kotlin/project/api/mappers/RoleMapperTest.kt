f0aafb60bc5301f3d827db91f633c0cca1bca8d1
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-08-13 00:32:26 +0300

    Finish UserMapperTest. May be changed in future by adding new tests

--- api/src/test/kotlin/project/api/mappers/RoleMapperTest.kt
+++ api/src/test/kotlin/project/api/mappers/RoleMapperTest.kt
@@ -1,16 +1,11 @@
 package project.api.mappers
 
-import org.checkerframework.checker.units.qual.A
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.stereotype.Component
-import org.springframework.test.context.bean.override.convention.TestBean
+
 import project.api.dto.RoleDto
 import project.api.mapper.role.RoleMapper
-import project.api.mapper.role.RoleMapperImpl
-import java.util.UUID
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
--- api/src/test/kotlin/project/api/mappers/UserMapperTest.kt
+++ api/src/test/kotlin/project/api/mappers/UserMapperTest.kt
@@ -2,22 +2,21 @@ package project.api.mappers
 
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import project.api.dto.UserDto
-import project.api.entity.Feedback
-import project.api.entity.Order
-import project.api.entity.Role
+import project.api.entity.*
 import project.api.mapper.user.UserMapperImpl
 import project.api.repository.feedback.FeedbackRepository
 import project.api.repository.order.OrderRepository
 import project.api.repository.role.RoleRepository
-import java.util.UUID
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
+import org.mockito.Mockito.`when`
+import java.util.*
+import kotlin.test.assertFailsWith
 
 @ExtendWith(MockitoExtension::class)
 class UserMapperTest {
@@ -39,7 +38,8 @@ class UserMapperTest {
     private lateinit var testRole: Role
     private lateinit var testOrder: Order
     private lateinit var testFeedback: Feedback
-
+    private lateinit var testProduct1: Product
+    private lateinit var testProduct2: Product
 
     @BeforeEach
     fun setUp(){
@@ -57,10 +57,45 @@ class UserMapperTest {
             id = roleId,
             name = "TEST_USER"
         )
-        //TODO: finish
+
+        testProduct1 = Product(
+            id = UUID.randomUUID(),
+            name = "Test name",
+            price = 300,
+            description = "Test description",
+            imageUrl = "https://imgBase:/testImg.com"
+        )
+
+        testProduct2 = Product(
+            id = UUID.randomUUID(),
+            name = "Test name 2",
+            price = 200,
+            description = "Test description 2",
+            imageUrl = "https://imgBase:/testImg2.com"
+        )
+
         testOrder = Order(
             id = orderId,
-            totalCost = 440
+            totalCost = 440,
+            products = mutableSetOf(
+                testProduct1,
+                testProduct2
+            )
+        )
+
+        val testUser = User(
+            id = UUID.randomUUID(),
+            username = "test_user",
+            email = "test_user@email.com",
+            password = "testPassword"
+        )
+
+        testFeedback = Feedback(
+            id = feedbackId,
+            review = "Test review",
+            rate = 4,
+            product = testProduct1,
+            user = testUser
         )
     }
 
@@ -77,6 +112,79 @@ class UserMapperTest {
         assertTrue(user.roles.isEmpty())
     }
 
-    //TODO: add new tests
+    @Test
+    fun testUserMapperShouldReturnExistingUserWithAllDependencies(){
+        `when`(orderRepository.findById(orderId)).thenReturn(Optional.of(testOrder))
+        `when`(feedbackRepository.findById(feedbackId)).thenReturn(Optional.of(testFeedback))
+        `when`(roleRepository.findById(roleId)).thenReturn(Optional.of(testRole))
 
+        val existUserDto = UserDto(
+            username = "test_username",
+            password = "testPassword123",
+            email = "testuser123@email.com",
+            orderIds = listOf(orderId),
+            feedbackIds = listOf(feedbackId),
+            roleIds = listOf(roleId)
+        )
+
+        val user = userMapper.toUser(existUserDto)
+
+        assertEquals("test_username", user.username)
+        assertEquals("testPassword123", user.password)
+        assertEquals("testuser123@email.com", user.email)
+
+        assertTrue(user.feedbacks.isNotEmpty())
+        assertEquals(orderId, user.orders.first().id)
+        assertEquals("TEST_USER", user.roles.first().name)
+
+        assertEquals("Test name", user.orders.first().products.first().name)
+    }
+
+    @Test
+    fun testUserMapperShouldFailsByWrongOrderId(){
+        val invalidOrderId = UUID.randomUUID()
+        `when`(orderRepository.findById(invalidOrderId)).thenThrow(IllegalArgumentException("Wrong Id!"))
+
+        val invalidOrderIdsUserDto = UserDto(
+            username = "test_username",
+            password = "testPassword123",
+            email = "testuser123@email.com",
+            orderIds = listOf(invalidOrderId),
+        )
+        assertFailsWith<IllegalArgumentException> {
+            userMapper.toUser(invalidOrderIdsUserDto)
+        }
+    }
+
+    @Test
+    fun testUserMapperShouldFailsByWrongRoleId(){
+        val invalidRoleId = UUID.randomUUID()
+        `when`(roleRepository.findById(invalidRoleId)).thenThrow(IllegalArgumentException("Wrong Id!"))
+
+        val invalidOrderIdsUserDto = UserDto(
+            username = "test_username",
+            password = "testPassword123",
+            email = "testuser123@email.com",
+            roleIds = listOf(invalidRoleId),
+        )
+        assertFailsWith<IllegalArgumentException> {
+            userMapper.toUser(invalidOrderIdsUserDto)
+        }
+    }
+
+    @Test
+    fun testUserMapperShouldFailsByWrongFeedbackId(){
+        val invalidFeedbackId = UUID.randomUUID()
+        `when`(feedbackRepository.findById(invalidFeedbackId)).thenThrow(IllegalArgumentException("Wrong Id!"))
+
+        val invalidOrderIdsUserDto = UserDto(
+            username = "test_username",
+            password = "testPassword123",
+            email = "testuser123@email.com",
+            feedbackIds = listOf(invalidFeedbackId),
+        )
+        assertFailsWith<IllegalArgumentException> {
+            userMapper.toUser(invalidOrderIdsUserDto)
+        }
+    }
 }
