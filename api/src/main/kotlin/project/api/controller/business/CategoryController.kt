fdbb7eceab3a1b62d5ee83e98bb27340aa15f5e5
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2025-09-03 13:32:29 +0300

    Add Category and Product controllers

---
+++ api/src/main/kotlin/project/api/controller/business/CategoryController.kt
@@ -0,0 +1,35 @@
+package project.api.controller.business
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import project.api.dto.business.CategoryDto
+import project.api.service.business.category.CategoryService
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/v1/categories")
+class CategoryController(private val categoryService: CategoryService) {
+
+    @PostMapping
+    fun addCategory(@RequestBody request: CategoryDto) = ResponseEntity.ok(categoryService.save(request))
+
+    @GetMapping
+    fun getAllCategories() = ResponseEntity.ok(categoryService.findAll())
+
+    @GetMapping("/{id}")
+    fun getCategoryById(@PathVariable id:UUID) = ResponseEntity.ok(categoryService.findById(id))
+
+    @PutMapping("/{id}")
+    fun updateCategoryById(@PathVariable id:UUID, @RequestBody request: CategoryDto) =
+        ResponseEntity.ok(categoryService.updateById(id, request))
+
+    @DeleteMapping("/{id}")
+    fun deleteCategoryById(@PathVariable id:UUID) = ResponseEntity.ok(categoryService.deleteById(id))
+}
---
+++ api/src/main/kotlin/project/api/controller/business/ProductController.kt
@@ -0,0 +1,40 @@
+package project.api.controller.business
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import project.api.dto.business.CategoryDto
+import project.api.dto.business.ProductDto
+import project.api.service.business.prodcut.ProductService
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/v1/products")
+class ProductController(private val productService: ProductService) {
+
+    @PostMapping
+    fun addProduct(@RequestBody request: ProductDto) = ResponseEntity.ok(productService.save(request))
+
+    @GetMapping
+    fun getAllProducts() = ResponseEntity.ok(productService.findAll())
+
+    @GetMapping("/by-category")
+    fun getProductsByCategory(@RequestBody request: CategoryDto) =
+        ResponseEntity.ok(productService.findProductsByCategory(request))
+
+    @GetMapping("/{id}")
+    fun getProductById(@PathVariable id: UUID) = ResponseEntity.ok(productService.findById(id))
+
+    @PutMapping("/{id}")
+    fun updateProductById(@PathVariable id: UUID, @RequestBody request: ProductDto) =
+        ResponseEntity.ok(productService.updateById(id, request))
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: UUID) = ResponseEntity.ok(productService.deleteById(id))
+}
